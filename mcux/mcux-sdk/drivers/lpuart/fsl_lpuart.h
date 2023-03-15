01776ab35f46b5efc6dbe5a616e8d14b3763900c
matt-wood-ct <matt.wood@corintech.com>
2023-03-15 20:24:39 +0000

    Updated MCUX files to align with 2b9354539e6e4f722749e87b0bdc22966dc080d9

--- mcux/mcux-sdk/drivers/lpuart/fsl_lpuart.h
+++ mcux/mcux-sdk/drivers/lpuart/fsl_lpuart.h
@@ -22,7 +22,7 @@
 /*! @name Driver version */
 /*@{*/
 /*! @brief LPUART driver version. */
-#define FSL_LPUART_DRIVER_VERSION (MAKE_VERSION(2, 5, 3))
+#define FSL_LPUART_DRIVER_VERSION (MAKE_VERSION(2, 7, 0))
 /*@}*/
 
 /*! @brief Retry times for waiting flag. */
@@ -327,7 +327,7 @@ extern const IRQn_Type s_lpuartIRQ[];
 #endif
 
 /* LPUART ISR for transactional APIs. */
-extern lpuart_isr_t s_lpuartIsr;
+extern lpuart_isr_t s_lpuartIsr[];
 
 /*******************************************************************************
  * API
@@ -650,9 +650,9 @@ uint32_t LPUART_GetEnabledInterrupts(LPUART_Type *base);
  * @param base LPUART peripheral base address.
  * @return LPUART data register addresses which are used both by the transmitter and receiver.
  */
-static inline uintptr_t LPUART_GetDataRegisterAddress(LPUART_Type *base)
+static inline uint32_t LPUART_GetDataRegisterAddress(LPUART_Type *base)
 {
-    return (uintptr_t) & (base->DATA);
+    return (uint32_t) & (base->DATA);
 }
 
 /*!
